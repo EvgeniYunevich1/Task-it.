72bda39a144df8e172607de52e44c37f0bd5ec38
Evgeni <yunevich_es@mail.ru>
2020-02-15 12:00:09 +0300

    создать число.определите,является ли последняя цифра числа семеркой.

---
+++ src/zadacha2.java
@@ -0,0 +1,11 @@
+public class zadacha2 {
+    public static void main(String[] args) {
+        int x = 207;
+        if (x % 10 == 7) {
+            System.out.println("является");
+        } else {
+            System.out.println("не является");
+        }
+
+    }
+}
