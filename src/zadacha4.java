bf310d56769deddd853b7e43aa9bc79af878d4ef
Evgeni <yunevich_es@mail.ru>
2020-02-15 12:26:37 +0300

    Имеются три числа -день,месяц,год.Вывести в виде трех чисел дату следующего дня.

---
+++ src/zadacha4.java
@@ -0,0 +1,9 @@
+public class zadacha4 {
+    public static void main(String[] args) {
+        int den=1;
+        int mes=2;
+        int god =2000;
+        den++;
+        System.out.println(den+"."+mes+"."+god);
+    }
+}
