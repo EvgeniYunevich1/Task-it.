5e8375414407f8761ad623853bcb6cade7b6e54e
Evgeni <yunevich_es@mail.ru>
2020-02-16 15:24:46 +0300

    Создать число.Определить,являеться ли последняя цифра числа семеркойю

--- src/zadacha3.java
+++ src/zadacha3.java
@@ -1,13 +1,11 @@
-    public class zadacha3 {
-        public static void main(String[] args) {
-            int a=6;
-            int b=10;
-            int r=5;
-            if(r>=a/2 & r>=b/2){
-                System.out.println("можно закрыть кругом");
-            } else{
-                System.out.println("нельзя закрыть");
-            }
+public class zadacha3 {
+    public static void main(String[] args) {
+        int x = 207;
+        if (x % 10 == 7) {
+            System.out.println("является");
+        } else {
+            System.out.println("не является");
         }
-    }
 
+    }
+}
