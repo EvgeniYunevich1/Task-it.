895db33245ed4109f983c69188c64fa2a08d24a0
Evgeni <yunevich_es@mail.ru>
2020-02-16 22:07:42 +0300

    Следущий день

--- src/zadacha6.java
+++ src/zadacha6.java
@@ -1,46 +1,46 @@
 public class zadacha6 {
     public static void main(String[] args) {
-        int d=28;
-        int m=2;
+        int d=31;
+        int m=3;
         int g=2020;
-        if(m==1 || m==3 || m==5 ||m==7 || m==8 || m==10){
-            if(!(d<1)&& !(d>30)){
+        if(m==1|| m==3|| m==5|| m==7|| m==8 ||m==10){
+            if(d>1 && d<31){
                 d++;
-                System.out.println(d+"."+m+"."+g+".");}
+                System.out.println(d+" "+m+" "+g);}
             else if(d==31){
                 d=1;
                 m++;
-                System.out.println(d+"."+m+"."+g+"."); }
+                System.out.println(d+" "+m+" "+g);}
+            }
+        if(m==4||m==6||m==9||m==11){
+            if(d>1 && d<30){
+                d++;
+                System.out.println(d+" "+m+" "+g);}
+            else if(d==30){
+                d=1;
+                m++;
+                System.out.println(d+" "+m+" "+g);}
+            }
+        if(m==2){
+            if(d>1 && d<29){
+                d++;
+                System.out.println(d+" "+m+" "+g); }
+            else if (d==29){
+                d=1;
+                m++;
+                System.out.println(d+" "+m+" "+g); }
         }
-         if(m==4||m==6||m==9||m==11){
-             if(!(d<1)&&!(d>29)){
-                 d++;
-                 System.out.println(d+"."+m+"."+g+"."); }
-             else if (d==30){
-                 d=1;
-                 m++;
-                 System.out.println(d+"."+m+"."+g+"."); }
-         }
-           if(m==12){
-               if(!(d<1)&&!(d>30)){
-                   d++;
-                   System.out.println(d+"."+m+"."+g+"."); }
-               else if (d==31){
-                   d=1;
-                   m=1;
-                   g++;
-                   System.out.println(d+"."+m+"."+g+"."); }
-           }
-           if(m==2){
-               if(!(d<1)&&!(d>27)){
-                   d++;
-                   System.out.println(d+"."+m+"."+g+"."); }
-               else if(d==28){
-                   d=1;
-                   m++;
-                   System.out.println(d+"."+m+"."+g+".");}
-
-               }
-           }
-
+        if(m==12){
+            if(d>1 && d<31){
+                d++;
+                System.out.println(d+" "+m+" "+g); }
+            else if(d==31){
+                d=1;
+                m=1;
+                g++;
+                System.out.println(d+" "+m+" "+g); }
+        }
     }
+}
+
+
