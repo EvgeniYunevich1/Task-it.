b9f80c67c34efdbe831d033cd7608ed2e8202aab
Evgeni <yunevich_es@mail.ru>
2020-02-16 17:30:05 +0300

    Рубли вывести в правельном подеже

--- src/zadacha5.java
+++ src/zadacha5.java
@@ -1,11 +1,16 @@
 public class zadacha5 {
-        public static void main(String[] args) {
-            int rybli =1;
-            int a=rybli%100;
-            int b=rybli%10;
-            if(b==1 & a!=11){
-                System.out.println(rybli+"рубль");}
-            if (a>1 && a<5 || b>1 && a<5){
-                System.out.println(rybli+"рубля");}
-            if(a<20 && a>9 || b>4 && b<10){
-                System.out.println(rybli+"рублей");}
+    public static void main(String[] args) {
+        int rybli =834;
+        int a = rybli % 100;
+        int b = rybli % 10;
+        if (b == 1 & a != 11) {
+            System.out.println(rybli + "рубль");
+        }
+        else if (a > 1 && a < 5 || b > 1 && b < 5) {
+            System.out.println(rybli + "рубля");
+        }
+        if (a < 20 && a > 9 || b > 4 && b < 10) {
+            System.out.println(rybli + "рублей");
+        }
+    }
+}
