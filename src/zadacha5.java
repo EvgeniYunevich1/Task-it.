9b63dd15d80aa8b548befac713492c539731bcb9
Evgeni <yunevich_es@mail.ru>
2020-02-16 18:25:54 +0300

    Рубли вывести в правильном падеже

--- src/zadacha5.java
+++ src/zadacha5.java
@@ -1,16 +1,16 @@
 public class zadacha5 {
     public static void main(String[] args) {
-        int rybli =834;
+        int rybli =934;
         int a = rybli % 100;
         int b = rybli % 10;
         if (b == 1 & a != 11) {
-            System.out.println(rybli + "рубль");
+            System.out.println(rybli +" " +"рубль");
         }
         else if (a > 1 && a < 5 || b > 1 && b < 5) {
-            System.out.println(rybli + "рубля");
+            System.out.println(rybli +" " +"рубля");
         }
         if (a < 20 && a > 9 || b > 4 && b < 10) {
-            System.out.println(rybli + "рублей");
+            System.out.println(rybli +" "+ "рублей");
         }
     }
 }
