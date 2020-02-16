7d45972b676dabb2976152629f2d634cfd61f743
Evgeni <yunevich_es@mail.ru>
2020-02-16 15:32:17 +0300

    Имеется прямоугольное отверстие размероми a и b, определите, можно ли его  полностью закрыть круглой картонкой радиусам r

--- src/zadacha4.java
+++ src/zadacha4.java
@@ -1,9 +1,11 @@
 public class zadacha4 {
     public static void main(String[] args) {
-        int den=1;
-        int mes=2;
-        int god =2000;
-        den++;
-        System.out.println(den+"."+mes+"."+god);
+        int a=15;
+        int b=6;
+        int r=10;
+        if( r>a/2 && r>b/2){
+            System.out.println("закроет");}
+        else{
+            System.out.println("не закроет");}
     }
 }
