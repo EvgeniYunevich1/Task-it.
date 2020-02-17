bf5a836a24fe11554476e6c89ba609e6b9f4d845
Evgeni <yunevich_es@mail.ru>
2020-02-17 23:47:33 +0300

    Помещаются ли дома

--- src/zadacha7.java
+++ src/zadacha7.java
@@ -1,15 +1,22 @@
 public class zadacha7 {
     public static void main(String[] args) {
-        int a=1;
-        int b=3;
-        int c = 2;
-        int d =4;
-        int e =5;
-        int f =4;
-        if(a+c<=e & b+d<=f) {
+        int a = 11;
+        int b = 5;
+        int c = 3;
+        int d = 5;
+        int e = 15;
+        int f = 5;
+        if ((a + c) <= e || (b + d) <= f) {
             System.out.println("можно");
-        }else{
+        } else if ((a + c) <= f || (b + d) <= e) {
+            System.out.println("можно");
+        } else if ((a + d) <= f || (b + c) <= e) {
+            System.out.println("можно");
+        } else if ((a + d) <= e || (b + c) <= f) {
+            System.out.println("можно");
+        } else {
             System.out.println("нельзя");
+
         }
     }
 }
