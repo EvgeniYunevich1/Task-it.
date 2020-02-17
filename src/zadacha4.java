cb398fa04c02774681920f17c8f5f4084b1d2521
Evgeni <yunevich_es@mail.ru>
2020-02-17 17:26:23 +0300

    закроет ли картонка

--- src/zadacha4.java
+++ src/zadacha4.java
@@ -1,13 +1,13 @@
 public class zadacha4 {
     public static void main(String[] args) {
-        int a=15;
-        int b=1;
-        int r=1;
-        int cE2=(a*a)+(b*b);
+        int a = 15;
+        int b = 1;
+        int r = 1;
+        int cE2 = (a * a) + (b * b);
         double c = Math.sqrt(cE2);
-        if(c<2*r) {
-            System.out.println("можно закрыть");}
-        else {
+        if (c < 2 * r) {
+            System.out.println("можно закрыть");
+        } else {
             System.out.println("нельзя закрыть");
 
         }
