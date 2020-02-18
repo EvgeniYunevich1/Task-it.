5ad1cee80bfd8c80f6c12bcf384157615f889cc9
Evgeni <yunevich_es@mail.ru>
2020-02-18 21:33:23 +0300

    10

---
+++ src/Lesson2/zadacha10.java
@@ -0,0 +1,18 @@
+package Lesson2;
+// Сумма цифр целого числа
+
+import java.util.Scanner;
+
+public class zadacha10 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Введите число");
+        int chislo = userInput.nextInt();
+        int sum = 0;
+        while (chislo > 0) {
+            sum += chislo % 10;
+            chislo /= 10;
+        }
+        System.out.println("Сумма чисел ="+sum);
+    }
+}
