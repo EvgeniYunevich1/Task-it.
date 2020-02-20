e0389cd10329b7a8ff6a96bae6b0b2ca26cfcea3
Evgeni <yunevich_es@mail.ru>
2020-02-20 17:48:04 +0300

    8

--- src/Lesson2/zadacha8.java
+++ src/Lesson2/zadacha8.java
@@ -1,16 +1,16 @@
 package Lesson2;
+
 //рассчитать факториал числа
 public class zadacha8 {
     public static void main(String[] args) {
-        long zadannoeChislo = 2;
+        long zadannoeChislo = 3;
         long factorial = 1;
         long i = 1;
         while (i <= zadannoeChislo) {
             factorial *= i;
             i++;
-            System.out.println(factorial);
-
         }
+        System.out.println(factorial);
     }
 }
 
