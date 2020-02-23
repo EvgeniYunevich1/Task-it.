096c0adeb05b08a481f1d1fffd0d736c0df0e05c
Evgeni <yunevich_es@mail.ru>
2020-02-23 16:46:44 +0300

    задача 17

--- src/Lesson3/zadacha17.java
+++ src/Lesson3/zadacha17.java
@@ -3,22 +3,22 @@ package Lesson3;
 public class zadacha17 {
     // Создать двухмерный квадратный массив,и заполнить его "бабочкой",т.е таким образом.
     public static void main(String[] args) {
-        int m[][] =new int[10][10];
-        for(int i=0;i<m.length/2;i++){
-            for(int j=0;j<m[i].length;j++){
-                if(((j<i)) || (j>=(m[i].length-i)))
-                    m[i][j]=0;
+        int m[][] = new int[10][10];
+        for (int i = 0; i < m.length / 2; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if (((j < i)) || (j >= (m[i].length - i)))
+                    m[i][j] = 0;
                 else
-                    m[i][j]=1;
+                    m[i][j] = 1;
                 System.out.print(m[i].length);
             }
         }
-        for(int i=m.length-1;i>=m.length/2;i--){
-            for(int j=0;j<m[i].length;j++){
-                if((j<(m[i].length-1-i) || (j>i)))
-                    m[i][j]=0;
+        for (int i = m.length - 1; i >= m.length / 2; i--) {
+            for (int j = 0; j < m[i].length; j++) {
+                if ((j < (m[i].length - 1 - i) || (j > i)))
+                    m[i][j] = 0;
                 else
-                    m[i][j]=1;
+                    m[i][j] = 1;
             }
         }
         for (int i = 0; i < m.length; i++) {
