e80d6146daff2fc58ab2c7626f6cf8ae9d8438ac
Evgeni <yunevich_es@mail.ru>
2020-02-23 16:41:36 +0300

    задача 14

--- src/Lesson3/zadacha14.java
+++ src/Lesson3/zadacha14.java
@@ -1,7 +1,7 @@
 package Lesson3;
 
-//В задаче на поиск максимальной оценки,вывести не саму оценку ,а ее номер.
 public class zadacha14 {
+    //В задаче на поиск максимальной оценки,вывести не саму оценку ,а ее номер.
     public static void main(String[] args) {
         int marks[] = {1, 10, 3, 4, 6, 5, 22, 3,};
         int maxIdex = 0;
