c533f1df9913cb7e718243cb10c5bc7f6f101f69
Evgeni <yunevich_es@mail.ru>
2020-02-23 19:53:24 +0300

    задача 16

--- src/Lesson3/zadacha16.java
+++ src/Lesson3/zadacha16.java
@@ -3,17 +3,19 @@ package Lesson3;
 public class zadacha16 {
     //Создать массив,заполнить его случайными элементами,распечатать ,перевернуть,и снова распечатать..
     public static void main(String[] args) {
-        int mas[] = new int[6];
+        int mas[] = new int[4];
         for (int i = 0; i < mas.length; i++) {
-            mas[i] = (int) (Math.random() * 2 + 1);
+            mas[i] = (int) (Math.random() * 10 + 1);
         }
+        System.out.println();
         for (int i = 0; i < mas.length; i++) {
-            System.out.print(mas[i]);
+            System.out.print(mas[i] + " ");
         }
         System.out.println();
         for (int i = mas.length - 1; i >= 0; i--) {
-            System.out.print(mas[i]);
+            System.out.print(mas[i] + " ");
         }
 
     }
 }
+
