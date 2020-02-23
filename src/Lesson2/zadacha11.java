07bfaf4e77294bc5b8e8dbcd85107c1c3f6f907a
Evgeni <yunevich_es@mail.ru>
2020-02-23 10:01:19 +0300

    задача 11

--- src/Lesson2/zadacha11.java
+++ src/Lesson2/zadacha11.java
@@ -3,9 +3,14 @@ package Lesson2;
 //Определить является ли число простым
 public class zadacha11 {
     public static void main(String[] args) {
-        int num = 211;
-        if (num / 1 == num & num / num == 1 & num % 2 != 0 & num % 3 != 0 & num % 5 != 0 & num % 7 != 0 || num == 2
-                || num == 7 || num == 3 || num == 5) {
+        int num = 5;
+        int delitel = 0;
+        for (int i = 1; i <= num; i++) {
+            if((num % i) == 0){
+            delitel++;
+            }
+        }
+        if (delitel == 2) {
             System.out.println("Простое число" + " " + num);
         } else {
             System.out.println("Составное число" + " " + num);
