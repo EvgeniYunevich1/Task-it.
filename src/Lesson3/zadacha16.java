ca0ae28e0d8a33e28b80f6d33b18a57868cccf28
Evgeni <yunevich_es@mail.ru>
2020-02-23 23:23:24 +0300

    задача 16

--- src/Lesson3/zadacha16.java
+++ src/Lesson3/zadacha16.java
@@ -6,16 +6,16 @@ public class zadacha16 {
         int mas[] = new int[4];
         for (int i = 0; i < mas.length; i++) {
             mas[i] = (int) (Math.random() * 10 + 1);
-            System.out.print(mas[i]+" ");
+            System.out.print(mas[i] + " ");
         }
         System.out.println();
-        for(int i=0;i<mas.length;i++){
-            if(i<mas.length/2){
-               int a=mas[i];
-                mas[i]=mas[mas.length-i-1];
-                mas[mas.length-i-1]=a;
+        for (int i = 0; i < mas.length; i++) {
+            if (i < mas.length / 2) {
+                int a = mas[i];
+                mas[i] = mas[mas.length - i - 1];
+                mas[mas.length - i - 1] = a;
             }
-            System.out.print(mas[i]+" ");
+            System.out.print(mas[i] + " ");
         }
 
 
