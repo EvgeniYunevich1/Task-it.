dbea5c3f7c833d12aa316765083aae0a14aca1c9
Evgeni <yunevich_es@mail.ru>
2020-02-23 16:48:28 +0300

    задача 11

--- src/Lesson2/zadacha11.java
+++ src/Lesson2/zadacha11.java
@@ -1,13 +1,13 @@
 package Lesson2;
 
-//Определить является ли число простым
+//Определить является ли число простым.
 public class zadacha11 {
     public static void main(String[] args) {
         int num = 5;
         int delitel = 0;
         for (int i = 1; i <= num; i++) {
-            if((num % i) == 0){
-            delitel++;
+            if ((num % i) == 0) {
+                delitel++;
             }
         }
         if (delitel == 2) {
