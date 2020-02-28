fb6a7ca4125167aaad4dd320e1094549b328be99
Evgeni <yunevich_es@mail.ru>
2020-02-28 21:45:09 +0300

    задача 19

--- src/Lesson5/Zadacha19.java
+++ src/Lesson5/Zadacha19.java
@@ -1,20 +1,26 @@
 package Lesson5;
 
 import java.util.Scanner;
+
 //Имеется строка с текстом.Подсчитать количество слов в тексте.
 public class Zadacha19 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку");
         String string = scanner.nextLine();
-        int n = 1;
+        int n = 0;
         string = string.trim();
+        if(string.length() !=0){
+            n++;
+        }
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) == ' ' && string.charAt(i + 1) != ' ') {
                 n++;
             }
         }
         System.out.println(n);
+
+
 
     }
 }
