6b5e18e34e3fc9d85395bf776474f75062a0f95b
EvgeniYunevich1 <yunevich_es@mail.ru>
2020-02-29 19:39:56 +0300

    задача 24

---
+++ src/Zadacha24/SmartTV.java
@@ -0,0 +1,24 @@
+package Zadacha24;
+
+public class SmartTV extends Texnika {
+    public void SerfInternet(){
+        System.out.println("искать в интернете" );
+    }
+    public void name(){
+        System.out.println(name);
+    }
+    protected boolean state= true;
+    public void on(){
+        state=true;
+    }
+    public void off(){
+        state=false;
+    }
+    public void printState(){
+        if(state){
+            System.out.println("TV On");
+        }else {
+            System.out.println("TV Off");
+        }
+    }
+}
---
+++ src/Zadacha24/Texnika.java
@@ -0,0 +1,13 @@
+package Zadacha24;
+
+public class Texnika {
+    String name="LG";
+    public void TVon(){
+        System.out.println("включен");
+    }
+    public void TVoff(){
+        System.out.println("выключен");
+    }
+
+
+}
