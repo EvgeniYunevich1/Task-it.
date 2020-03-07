762af2530470ec7eb88a05347bedf14eaf6def38
EvgeniYunevich1 <yunevich_es@mail.ru>
2020-03-07 21:35:32 +0300

    задача 22

--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Bankomat bankomat=new Bankomat(100,100,100);
-        bankomat.addCash(200,200,2000);
-        bankomat.getCash(120);
-
-    }
-}
